d6cd2fd4e2ee443d17885280eeb2160a812cf2a4
natrix_dev <discordlenko@gmail.com>
2022-02-04 13:20:19 +0100

    Create script.py

---
+++ script.py
@@ -0,0 +1,33 @@
+# import some libraries 
+
+from book_detector import dec
+
+# init a webcam 
+
+dec.init(NEW_WEBCAM: 12px; 45px;){
+  cam.name("1")
+}
+
+dec.cam-1.CONNECT 
+
+dec.CONNECT==TRUE 
+
+# record to class in a file 
+dec.record("filter-'books')
+           
+           import alfa as *
+           # for a to z letters
+           books.* 
+           
+           filter(alfa.a-to-z)=>[BOOKS]
+           #init filter
+           
+           dec.new(class.txt) 
+           file.write('BOOKS')
+           # new file called class.txt will contain all the books 
+           
+           
+    dec.stop
+           alfa.stop
+                write.stop()
+        # stop program 
